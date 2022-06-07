e8d52ec6595a67e4fbe6e435f382b79bf86bb8bb
Levi Shevick <levidshevick@gmail.com>
2022-06-07 13:14:18 -0400

    adds random word generator from list of words

---
+++ main.py
@@ -0,0 +1,5 @@
+import random
+words = ['fish', 'food', 'represent', 'quick', 'edible', 'imaginary', 'lost', 'duck', 'lettuce', 'magic', 'mandatory', 'latent', 'escapade', 'garage', 'friends', 'search', 'match', 'loop', 'lamp', 'loot', 'steal', 'internet', 'grass', 'rocket', 'blade', 'swim', 'sword', 'axe', 'eye', 'knife', 'jar', 'rifle', 'revolver', 'sentry', 'dispenser', 'robot', 'baseball']
+
+word = random.choice(words)
+print(word)
