2a7bfd80da3e980684220c283410c393a5ebfbbd
Levi Shevick <levidshevick@gmail.com>
2022-06-07 15:49:12 -0400

    adds hangman ascii art

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from hangman_ascii import HANGMANPICS
 words = ['fish', 'food', 'represent', 'quick', 'edible', 'imaginary', 'lost', 'duck', 'lettuce', 'magic', 'mandatory', 'latent', 'escapade', 'garage', 'friends', 'search', 'match', 'loop', 'lamp', 'loot', 'steal', 'internet', 'grass', 'rocket', 'blade', 'swim', 'sword', 'axe', 'eye', 'knife', 'jar', 'rifle', 'revolver', 'sentry', 'dispenser', 'robot', 'baseball']
 
 word = random.choice(words)
@@ -8,9 +9,8 @@ test = 'flavor'
 
 mystery = list(word)
 
-
+# global variables
 turns = 6
-
 correct = ''
 incorrect = ''
 blanks = []
@@ -24,37 +24,39 @@ def eval_letter(user):
         global correct, blanks
         correct += user
         print('You got it!')
+        print('*****************************************************')
     else:
         global turns, incorrect
         turns -= 1
         incorrect += user
         print('Nope, wrong letter. Try again!')
+        print('*****************************************************')
 
 def display_word(mystery):
     global blanks
-    blanks = '_' * len(mystery)
     for l in range(len(mystery)):
         if mystery[l] in correct:
             blanks = blanks[:l] + mystery[l] + blanks[l+1:]
-    print(''.join(blanks))
     print('')
+    print(''.join(blanks))
     print('')
     # print(mystery)
     return blanks
 
+# this code displays blanks at start of game so users know how long the word is 
 blanks = '_' * len(mystery)
-
 print(''.join(blanks))
 
 while turns > 0:
-    print(f"Remaining Turns: {turns}")
+    print(HANGMANPICS[turns])
     print(f"Incorrect letters: {incorrect}")
+    print(' ')
     user = input('Try to guess a letter in this mystery word: ')
 
     eval_letter(user)
     display_word(mystery)
 
     if blanks == word:
-        print('you won!')
+        print('✨ you won! ✨')
         break
 
