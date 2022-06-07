5872d5c260d3c9fbe50a5a627968c4fa28ae7ca1
Levi Shevick <levidshevick@gmail.com>
2022-06-07 13:40:04 -0400

    adds function to display word as blanks

--- main.py
+++ main.py
@@ -2,4 +2,20 @@ import random
 words = ['fish', 'food', 'represent', 'quick', 'edible', 'imaginary', 'lost', 'duck', 'lettuce', 'magic', 'mandatory', 'latent', 'escapade', 'garage', 'friends', 'search', 'match', 'loop', 'lamp', 'loot', 'steal', 'internet', 'grass', 'rocket', 'blade', 'swim', 'sword', 'axe', 'eye', 'knife', 'jar', 'rifle', 'revolver', 'sentry', 'dispenser', 'robot', 'baseball']
 
 word = random.choice(words)
-print(word)
+# print(word)
+
+test = 'stand'
+
+def display_word(test):
+    word = list(test)
+    blank = []
+    # print(word)
+    for char in word:
+        res = '_'
+        blank += res
+    print(''.join(blank))
+    return ''.join(blank)
+
+
+
+display_word(test)
